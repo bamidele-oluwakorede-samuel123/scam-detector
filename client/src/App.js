e30880f57407494ee89412a92a80d089d008ec57
ObsidianMind-Tech <bamideleos.24@student.funaab.edu.ng>
2026-04-11 01:31:58 -0800

    fixed client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Scam Detector</h1>
+      <p>Welcome to the Scam Detector!</p>
+      <p>Use this application to detect potential scams.</p>
+      </div>
+  );
+}
+
+export default App;
